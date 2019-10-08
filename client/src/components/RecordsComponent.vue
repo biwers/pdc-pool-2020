d7a4fbceb17a57365661e9859eb5ddd17dfa8411
Noah Boulton <noah.boulton@gmail.com>
2019-10-08 17:01:18 -0400

    Updating records page

--- client/src/components/RecordsComponent.vue
+++ client/src/components/RecordsComponent.vue
@@ -4,6 +4,7 @@
       <hr>
       <p><strong>Most Points:</strong> Noah (The Kevin Gravel Grinders) – 129 points (2018)</p>
       <p><strong>Fewest Points:</strong> Luke (Moves Like Jagr) – 19 points (2019)</p>
+      <p><strong>Most Ho's with 0 Points:</strong> Esrah (Mo’s Top Ho’s) – 4 players (2019)</p>
   </div>
 </template>
 
