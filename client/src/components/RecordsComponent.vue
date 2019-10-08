8f0182e6889b17e8b80a303925ba59a35bed5046
Noah Boulton <noah.boulton@gmail.com>
2019-10-08 17:55:24 -0400

    Updating records page

--- client/src/components/RecordsComponent.vue
+++ client/src/components/RecordsComponent.vue
@@ -4,7 +4,9 @@
       <hr>
       <p><strong>Most Points:</strong> Noah (The Kevin Gravel Grinders) – 129 points (2018)</p>
       <p><strong>Fewest Points:</strong> Luke (Moves Like Jagr) – 19 points (2019)</p>
-      <p><strong>Most Ho's with 0 Points:</strong> Esrah (Mo’s Top Ho’s) – 4 players (2019)</p>
+      <p><strong>Most Zeroed Out Players:</strong> Esrah (Mo’s Top Ho’s) – 4 players (2019)</p>
+      <p><strong>Most Points after Round 1:</strong> Noah (The Kevin Gravel Grinders) – 52 points (2018)</p>
+      <p><strong>Most Players in a Stanley Cup Final:</strong>  Ian (A Hope and a Dream) - 5 players (2018)</p>
   </div>
 </template>
 
