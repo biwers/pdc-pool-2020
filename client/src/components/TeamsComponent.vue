72c3a3415a645e76bd175b7dd48dccc93226904d
Noah Boulton <noah.boulton@gmail.com>
2019-10-08 16:38:35 -0400

    Updated player picture link so it works for more players

--- client/src/components/TeamsComponent.vue
+++ client/src/components/TeamsComponent.vue
@@ -28,61 +28,61 @@
           <tr>
             <td>F</td>
             <td>{{team.team.forwards.f1.name}}</td>
-            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.forwards.f1.id}@2x.jpg`" width=75 height=75></td>
+            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.forwards.f1.id}.jpg`" width=75 height=75></td>
             <td>{{team.team.forwards.f1.points}}</td>
           </tr>
           <tr>
             <td>F</td>
             <td>{{team.team.forwards.f2.name}}</td>
-            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.forwards.f2.id}@2x.jpg`" width=75 height=75></td>
+            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.forwards.f2.id}.jpg`" width=75 height=75></td>
             <td>{{team.team.forwards.f2.points}}</td>
           </tr>
           <tr>
             <td>F</td>
             <td>{{team.team.forwards.f3.name}}</td>
-            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.forwards.f3.id}@2x.jpg`" width=75 height=75></td>
+            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.forwards.f3.id}.jpg`" width=75 height=75></td>
             <td>{{team.team.forwards.f3.points}}</td>
           </tr>
           <tr>
             <td>F</td>
             <td>{{team.team.forwards.f4.name}}</td>
-            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.forwards.f4.id}@2x.jpg`" width=75 height=75></td>
+            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.forwards.f4.id}.jpg`" width=75 height=75></td>
             <td>{{team.team.forwards.f4.points}}</td>
           </tr>
           <tr>
             <td>F</td>
             <td>{{team.team.forwards.f5.name}}</td>
-            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.forwards.f5.id}@2x.jpg`" width=75 height=75></td>
+            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.forwards.f5.id}.jpg`" width=75 height=75></td>
             <td>{{team.team.forwards.f5.points}}</td>
           </tr>
           <tr>
             <td>D</td>
             <td>{{team.team.defensemen.d1.name}}</td>
-            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.defensemen.d1.id}@2x.jpg`" width=75 height=75></td>
+            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.defensemen.d1.id}.jpg`" width=75 height=75></td>
             <td>{{team.team.defensemen.d1.points}}</td>
           </tr>
           <tr>
             <td>D</td>
             <td>{{team.team.defensemen.d2.name}}</td>
-            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.defensemen.d2.id}@2x.jpg`" width=75 height=75></td>
+            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.defensemen.d2.id}.jpg`" width=75 height=75></td>
             <td>{{team.team.defensemen.d2.points}}</td>
           </tr>
           <tr>
             <td>D</td>
             <td>{{team.team.defensemen.d3.name}}</td>
-            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.defensemen.d3.id}@2x.jpg`" width=75 height=75></td>
+            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.defensemen.d3.id}.jpg`" width=75 height=75></td>
             <td>{{team.team.defensemen.d3.points}}</td>
           </tr>
           <tr>
             <td>G</td>
             <td>{{team.team.goalies.g1.name}}</td>
-            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.goalies.g1.id}@2x.jpg`" width=75 height=75></td>
+            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.goalies.g1.id}.jpg`" width=75 height=75></td>
             <td>{{team.team.goalies.g1.points}}</td>
           </tr>
           <tr>
             <td>G</td>
             <td>{{team.team.goalies.g2.name}}</td>
-            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.goalies.g2.id}@2x.jpg`" width=75 height=75></td>
+            <td><img :src="`https://nhl.bamcontent.com/images/headshots/current/168x168/${team.team.goalies.g2.id}.jpg`" width=75 height=75></td>
             <td>{{team.team.goalies.g2.points}}</td>
           </tr>
         </table>
